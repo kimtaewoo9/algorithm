d0c1ea7690d71cb06675e0cb77e06c8dd3ea52c1
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-11-11 16:34:35 +0900

    1912.cpp

--- DP/1912.cpp
+++ DP/1912.cpp
@@ -19,12 +19,7 @@ int main(){
     arr[0] = board[0];
     int mx=-0x7fffffff;
     for(int cnt_i=1;cnt_i<n;cnt_i++){
-        if(arr[cnt_i-1] + board[cnt_i] > 0){
-            arr[cnt_i] = arr[cnt_i-1]+board[cnt_i];
-        }
-        else{
-            arr[cnt_i] = arr[cnt_i-1];
-        }
+        arr[cnt_i] = max(arr[cnt_i-1]+board[cnt_i], board[cnt_i]);
     }
 
     int ans=-0x7fffffff;
